443c239e0d5d84c5cd5f061b653147d860bbc9dd
Brendan Muldoon <brendan.muldoon@bt.com>
2022-08-05 16:53:26 +0100

    added identity module

---
+++ identity-bc/src/test/java/com/example/identitybc/IdentityBcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.identitybc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdentityBcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
